c3c482ccab73dfe5169b94f7ea7d517088ae5822
Ben Scholar <bens@digitalman.com>
2017-10-24 16:09:33 -0700

    Timed Center and Timed Baseline autos

---
+++ src/org/team5499/robots/frc2017/commands_timed/TimedDriveCommand.java
@@ -0,0 +1,42 @@
+package org.team5499.robots.frc2017.commands_timed;
+
+import org.team5499.robots.frc2017.commands.GenericCommand;
+import org.team5499.robots.frc2017.subsystems.Subsystems;
+import edu.wpi.first.wpilibj.Timer;
+
+public class TimedDriveCommand extends GenericCommand {
+
+    private double duration;
+    private double speed;
+    private double startTime;
+
+    public TimedDriveCommand(double to, double dur, double speed) {
+        super(to);
+        duration = dur;
+        this.speed = speed;
+    }
+
+    @Override
+    public void start() {
+        super.start();
+        startTime = Timer.getFPGATimestamp();
+        Subsystems.drivetrain.drive(speed, speed);
+    }
+
+    public void handle() {
+        if(Timer.getFPGATimestamp() - startTime >= duration){
+            Subsystems.drivetrain.stop();
+        }
+    }
+
+    @Override
+    public boolean isFinished() {
+        return super.isFinished();
+    }
+
+    @Override
+    public void reset() {
+
+    }
+
+}
--- src/org/team5499/robots/frc2017/controllers/AutoController.java
+++ src/org/team5499/robots/frc2017/controllers/AutoController.java
@@ -7,20 +7,28 @@ import org.team5499.robots.frc2017.commands.DoNothingCommand;
 import org.team5499.robots.frc2017.commands.TurnCommand;
 import org.team5499.robots.frc2017.commands.Routine;
 import org.team5499.robots.frc2017.commands.GenericCommand;
+import org.team5499.robots.frc2017.commands_timed.TimedDriveCommand;
 
 
 public class AutoController {
 
     private Routine center, left, right, test;
+    private Routine timedCenter, timedBaseline;
     private Routine currRoutine;
     private int autoChoice;
 
     public AutoController() {
 
+        // PID routines
         center = new Routine();
         left = new Routine();
         right = new Routine();
         test = new Routine();
+
+        // Timed Routines
+        timedCenter = new Routine();
+        timedBaseline = new Routine();
+
         // center auto
         center.addCommand(new DriveCommand(5,80));
         center.addCommand(new GearmechCommand(1, GearmechCommand.Direction.DOWN));
@@ -38,7 +46,15 @@ public class AutoController {
         // Test
         test.addCommand(new DoNothingCommand(2));
 
-        autoChoice = 0;
+        // Timed Center Auto
+        timedCenter.addCommand(new TimedDriveCommand(5, 2, 0.2));
+        timedCenter.addCommand(new GearmechCommand(2, GearmechCommand.Direction.DOWN));
+        timedCenter.addCommand(new TimedDriveCommand(5, 2, -0.2));
+
+        // Timed Center Baseline
+        timedBaseline.addCommand(new TimedDriveCommand(10, 10, 0.4));
+
+        autoChoice = 4;
     }
 
     public void Start() {
@@ -60,6 +76,14 @@ public class AutoController {
             case 3:
                 currRoutine = test;
                 System.out.println("Test auto selected");
+                break;
+            case 4: 
+                currRoutine = timedCenter;
+                System.out.println("Timed Center Auto selected");
+                break;
+            case 5:
+                currRoutine = timedBaseline;
+                System.out.println("Timed Baseline Auto Selected");
                 break;
             default:
                 System.err.println("ERROR: Auto selected not found");
