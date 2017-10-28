20d56cb692d41613ba50b89b8b0c80da439ef88b
Andy <and164v@gmail.com>
2017-10-27 18:48:02 -0700

    added pid correction for the turn command - should make both wheels turn the same amount when turning

--- src/org/team5499/robots/frc2017/Commands/TurnCommand.java
+++ src/org/team5499/robots/frc2017/Commands/TurnCommand.java
@@ -7,6 +7,8 @@ import org.team5499.robots.frc2017.subsystems.*;
 public class TurnCommand extends GenericCommand {
 
     private double setPoint;
+    private double initLeftValue;
+    private double initRightValue;
 
     public TurnCommand(double to, double sp) {
         super(to);
@@ -18,9 +20,24 @@ public class TurnCommand extends GenericCommand {
         super.start();
         Subsystems.anglePID.setPID(Reference.kAP, Reference.kAI, Reference.kAD);
         Subsystems.anglePID.setSetpoint(setPoint);
+
+        initLeftValue = Subsystems.encoders.getLeftDistance();
+        initRightValue = Subsystems.encoders.getRightDistance();
     }
 
     public void handle() {
+        // (Turn left)positive if right distance is greater than left distance
+        // (Turn right)positive if left distance is greater than right distance
+        double offset = (Subsystems.encoders.getLeftDistance()-initLeftValue)+(Subsystems.encoders.getRightDistance()-initRightValue);
+
+        Subsystems.leftPID.setSetpoint((Subsystems.encoders.getLeftDistance()-initLeftValue)-(offset/2));
+        Subsystems.rightPID.setSetpoint((Subsystems.encoders.getRightDistance()-initRightValue)-(offset/2));
+
+        Subsystems.leftPID.setInput(Subsystems.encoders.getLeftDistance());
+        Subsystems.rightPID.setInput(Subsystems.encoders.getRightDistance());
+
+        Subsystems.leftPID.calculate();
+        Subsystems.rightPID.calculate();
 
         Subsystems.anglePID.setInput(Subsystems.angle.getAngle());
 
@@ -28,12 +45,11 @@ public class TurnCommand extends GenericCommand {
         double driveOutput = Subsystems.anglePID.getOutput();
         
         //System.out.println("Actual:" + driveOutput);
-
         if(Math.abs(driveOutput) > 0.2) {
             driveOutput = (driveOutput < 0)?-0.2:0.2;
         }
 
-        Subsystems.drivetrain.drive(-driveOutput, driveOutput);
+        Subsystems.drivetrain.drive(-driveOutput-Subsystems.leftPID.getOutput(), driveOutput-Subsystems.rightPID.getOutput());
     }
 
     @Override
